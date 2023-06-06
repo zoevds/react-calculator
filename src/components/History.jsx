9c957abf8223a6d39789670cb3478bf39136dbe1
zoevds <zoevanderschyff13@gmail.com>
2023-06-06 09:36:28 +0200

    added history file

---
+++ src/components/History.jsx
@@ -0,0 +1,13 @@
+import {useCalculator} from "../contexts/calculator.jsx";
+
+const History = () => {
+    const calculator = useCalculator();
+
+    return (
+        <>
+            <h1>History</h1>
+        </>
+    )
+};
+
+export default History;
