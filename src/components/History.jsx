1a8138b6e59e5bd1e010cf206c0d8e624cf5430a
zoevds <zoevanderschyff13@gmail.com>
2023-06-06 16:05:41 +0200

    added calculator history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -6,6 +6,7 @@ const History = () => {
     return (
         <>
             <h1>History</h1>
+            {calculator.history.map((item, i) => <p key={`history_${i}`}>{item.cal}</p>)}
         </>
     )
 };
